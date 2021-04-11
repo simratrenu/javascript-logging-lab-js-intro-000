35fe25fde2236f2e07ff77e8548eaaaeaaab3253
simrat k renu <simratrenu@gmail.com>
2021-04-11 22:43:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 console.error("HALP!")
 console.log("I would be a logger.")
+console.log(...)
+console.warn(...)
+console.warn(warnings)
+console.warn("I would have been a logger.")
+console.warn("unexpected")
+console.warn
